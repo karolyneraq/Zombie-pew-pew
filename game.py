f1ba502ed49577412002be131cfae46aa7f4382c
Dário Souza <drio.srf@gmail.com>
2023-02-26 22:56:53 -0400

    Creating a player 2 in the game

--- game.py
+++ game.py
@@ -21,9 +21,11 @@ pygame.display.set_caption("Zombie Pew Pew")
 font = pygame.font.Font('assets/PressStart2P.ttf', 45)
 
 # players
-cow1 = Player("assets/char_1red/", (325, 300))
+cow1 = Player("assets/char_1red/", (325, 250))
+cow2 = Player("assets/char2_blue/", (325, 350))
 player_group = pygame.sprite.Group()
 player_group.add(cow1)
+player_group.add(cow2)
 
 # Zombie
 zombie = Zombie("assets/zombie/", (650, 150))
@@ -52,9 +54,13 @@ while loop:
     # Movement Keys
     cow1.set_movement(pygame.joystick.Joystick(0))
     cow1.set_fire(pygame.joystick.Joystick(0))
+    cow2.set_movement(pygame.joystick.Joystick(0))
+    cow2.set_fire(pygame.joystick.Joystick(0))
 
     # Moving the objects
     for bullet in cow1.get_bullets():
+        bullet.move()
+    for bullet in cow2.get_bullets():
         bullet.move()
 
     # Showing everything
@@ -62,21 +68,23 @@ while loop:
 
     if choice_scenario == 1:
         scenario1.draw_scenario()
-        cow1.set_obstacles(obstacles1)
-        cow1.set_zombies(zombie_group)
+        for player in player_group:
+            player.set_obstacles(obstacles1)
+            player.set_zombies(zombie_group)
         for monstro in zombie_group:
             monstro.set_obstacles(obstacles1)
     else:
         scenario2.draw_scenario()
-        cow1.set_obstacles(obstacles2)
-        cow1.set_zombies(zombie_group)
+        for player in player_group:
+            player.set_obstacles(obstacles2)
+            player.set_zombies(zombie_group)
         for monstro in zombie_group:
             monstro.set_obstacles(obstacles2)
 
     zombie_group.draw(screen)
     player_group.draw(screen)
     cow1.get_bullets().draw(screen)
-
+    cow2.get_bullets().draw(screen)
     zombie_group.update()
     player_group.update()
 
